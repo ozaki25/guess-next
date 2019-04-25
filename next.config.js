f8800395cb43c9e75b358e977bc7c795a7ad1307
ozaki <fs65ugumori@yahoo.co.jp>
2019-04-26 08:00:58 +0900

    add config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const { GuessPlugin } = require('guess-webpack');
+
+module.exports = {
+  webpack: function(config, { isServer }) {
+    if (isServer) return config;
+    config.plugins.push(
+      new GuessPlugin({
+        reportProvider() {
+          return Promise.resolve(
+            JSON.parse(require('fs').readFileSync('./routes.json')),
+          );
+        },
+      }),
+    );
+    return config;
+  },
+};
